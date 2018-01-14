7ab74262716a6fe079cbcf05454cbf0a70ecd430
Afgan Khalilov <hov-dev@protonmail.ch>
2018-01-14 17:02:33 +0400

    test

--- public/js/app.js
+++ public/js/app.js
@@ -753,16 +753,10 @@ module.exports = __webpack_require__(38);
 /* 8 */
 /***/ (function(module, exports, __webpack_require__) {
 
-<<<<<<< HEAD
-__webpack_require__(9);
-
-__webpack_require__(35);
-=======
 // bootstrap
 __webpack_require__(9);
 
 $(document).ready(function () {
->>>>>>> hovdev1401
 
   //стартовый скрипт
   __webpack_require__(35);
@@ -833,67 +827,6 @@ if (token) {
 // });
 
 /***/ }),
-<<<<<<< HEAD
-/* 9 */
-/***/ (function(module, exports, __webpack_require__) {
-
-
-window._ = __webpack_require__(10);
-
-/**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
- */
-
-try {
-  window.$ = window.jQuery = __webpack_require__(13);
-
-  __webpack_require__(14);
-} catch (e) {}
-
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
-
-window.axios = __webpack_require__(15);
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-/**
- * Next we will register the CSRF Token as a common header with Axios so that
- * all outgoing HTTP requests automatically have it attached. This is just
- * a simple convenience so we don't have to attach every token manually.
- */
-
-var token = document.head.querySelector('meta[name="csrf-token"]');
-
-if (token) {
-  window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-} else {
-  console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
-}
-
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
-
-// import Echo from 'laravel-echo'
-
-// window.Pusher = require('pusher-js');
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: 'your-pusher-key'
-// });
-
-/***/ }),
-=======
->>>>>>> hovdev1401
 /* 10 */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -31766,15 +31699,12 @@ module.exports = function spread(callback) {
 /***/ }),
 /* 35 */
 /***/ (function(module, exports) {
-<<<<<<< HEAD
-=======
 
 Api = {
 
     prepare: function prepare() {
         Api.boot();
     },
->>>>>>> hovdev1401
 
     boot: function boot() {
 
@@ -31875,14 +31805,6 @@ Api = {
 
     //-------------------- FUNCTIONS  ----------------------------//
 
-<<<<<<< HEAD
-/***/ }),
-/* 36 */,
-/* 37 */,
-/* 38 */
-/***/ (function(module, exports) {
-
-=======
 
     addChits: function addChits() {
         var chitsAddress = $("#chits-address-input").val();
@@ -32109,7 +32031,6 @@ Route = {
 /* 38 */
 /***/ (function(module, exports) {
 
->>>>>>> hovdev1401
 // removed by extract-text-webpack-plugin
 
 /***/ })
