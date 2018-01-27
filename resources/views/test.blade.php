5140e50d4d490832d81abb75ae89e97e32065d6a
Afgan Khalilov <hov-dev@protonmail.ch>
2018-01-27 20:40:52 +0400

    test

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -2,96 +2,180 @@
 @section('content')
     @include('layouts.includes.navbar')
 
-    <!-- 1. The <iframe> (and video player) will replace this <div> tag. -->
-
     <style>
+
+    body {
+        margin-top:80px;
+        padding:20px;
+    }    
     .chit {
+        float:left;
         margin: auto;
         height:200px;
         width: 200px;
         background:yellow;
         cursor:pointer;
+        margin:10px;
     }
+    .chit2 {
+        margin: auto;
+        height:200px;
+        width: 200px;
+        background:green;
+        cursor:pointer;
+        float:left;
+        margin:10px;
 
+    }
     </style>
 
 
-    <div id="chit" class="chit">
+    <input class="playlist" type="text"/>
 
+    <div class="chit-list">
+        <div id="chit" class="chit chit-code-GvbQzRAi4wM">
+            <div class="playerblock" id="custom-player-id-GvbQzRAi4wM" data-video="GvbQzRAi4wM">
+            </div>
+        </div>
+        <div id="chit2" class="chit chit-code-HCm6gRHINqA ">
+            <div class="playerblock" id="custom-player-id-HCm6gRHINqA" data-video="HCm6gRHINqA">
+            </div>
+        </div>   
     </div>
 
-    <div id="player-parent">
-        <div id="player"></div>
-    </div>
 
-    <div id="custom-player">
+    <!-- JavaScript -->
 
-    </div>
+      
+<script>
 
 
-
-    <!-- <iframe id="player" type="text/html" width="640" height="360"
-      src="http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com"
-      frameborder="0"></iframe> -->
-
-      <script>
-      // 2. This code loads the IFrame Player API code asynchronously.
       var tag = document.createElement('script');
-
       tag.src = "https://www.youtube.com/iframe_api";
       var firstScriptTag = document.getElementsByTagName('script')[0];
       firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-      // 3. This function creates an <iframe> (and YouTube player)
-      //    after the API code downloads.
       var player;
-      function onYouTubeIframeAPIReady() {
-          alert('t');
-
-          $('#chit').click(function() {
-              alert('click');
-
-              var html = $('#player-parent').html();
-              alert(html);
-                // player.stopVideo();
-
-                $('#custom-player').html(html);
-          })
 
 
 
+    function enableAutoPlay(videoId) {
 
-              player = new YT.Player('player', {
-                height: '360',
-                width: '640',
-                videoId: '2Vv-BfVoq4g',
+        //alert(videoId);
+
+        var dataBlock = $('.chit-list').find('div#custom-player-id-' + videoId);
+        var dataCode = dataBlock.data('video');
+        //alert(dataCode);
+        
+
+        var dataParent = $(dataBlock).parent();
+        var dataParentId = $(dataParent).attr('id');
+        var dataParentClass = $(dataParent).attr('class');
+
+        //alert(dataParentId);
+        //alert(dataParentClass);
+
+
+        var nextChitId = $("div.chit-code-" + dataCode).next().attr('id');
+
+        $('#' + nextChitId).click();
+
+        return false;
+    }
+
+        $(".playlist").change(function(){
+
+            var videoId = $(".playlist").val();
+            setTimeout(function(){
+                
+                enableAutoPlay(videoId);
+
+
+
+                }, 1000);
+        });
+
+
+
+        $('.chit').click(function() {         
+
+            // prod
+
+            //alert($(this).attr('id'));
+
+
+            var playerblockId = $(this).find("div.playerblock").attr('id');
+            var playerVideoId = $(this).find("div.playerblock").data('video');
+            var playerblockParent = $(this).attr('id');
+
+
+        
+            if (typeof player != "undefined") {
+                //player.destroy();
+                $('#player').remove();
+            }
+
+
+            
+            var playerblock = $(this).find('.playerblock');
+            playerblock.append('<div id="player">' + '</div>');
+
+
+            //var myDiv1Para = $('#player-c');
+            //myDiv1Para.attr('id', 'player');
+            //myDiv1Para.appendTo('#' + playerblockId);
+
+
+            //var myDiv1Para = $('#player').remove();
+            //myDiv1Para.appendTo('#' + playerblockId);
+
+            
+            player = new YT.Player('player', {
+                height: '200',
+                width: '200',
+                videoId: playerVideoId,
                 events: {
-                  'onReady': onPlayerReady,
-                  'onStateChange': onPlayerStateChange
+                    'onReady': onPlayerReady,
+                    'onStateChange': onPlayerStateChange
                 }
-          });
-      }
+            });
+        })
 
-      // 4. The API will call this function when the video player is ready.
       function onPlayerReady(event) {
-          alert('ready');
-        // event.target.playVideo(); //auto play video
+        //alert('ready');
+        player.playVideo();
       }
 
-      // 5. The API calls this function when the player's state changes.
-      //    The function indicates that when playing a video (state=1),
-      //    the player should play for six seconds and then stop.
+
       var done = false;
       function onPlayerStateChange(event) {
-        if (event.data == YT.PlayerState.PLAYING && !done) {
-          setTimeout(stopVideo, 6000);
-          done = true;
+
+        // если видео закончилось, включаем автопроигрывание
+        var state = player.getPlayerState()
+        //alert(state);
+        if(state == 0) {
+            // берем id текущего трэка
+            var videoId = player.getVideoData()['video_id'];
+            // удаляем текущий плеер
+            player.destroy();
+
+            // передаем управление функции автопроигрывания
+            //enableAutoPlay(videoId);
+            $('.playlist').val(videoId);
+            $(".playlist").trigger("change");
+
         }
+        
       }
-      function stopVideo() {
+    function stopVideo() {
         player.stopVideo();
-      }
-    </script>
+    }
+
+    function playVideo() {
+        player.playVideo();
+    }
+
+
+</script>
 
 
 
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,9 @@
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+//  Route::get('/', function () {
+//      return view('welcome');
+//  });
 
 // Testing
 Route::any('/api/test', 'TestController@test');
