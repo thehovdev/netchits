ad9419a831fc8360fcdbb6dd213b51612963007c
Afgan Khalilov <hov-dev@protonmail.ch>
2018-02-15 03:13:41 +0400

    fixes

--- resources/views/layouts/includes/friends-list.blade.php
+++ resources/views/layouts/includes/friends-list.blade.php
@@ -8,22 +8,24 @@
         @endif
 
     </div>
-    @foreach($friends as $friend)
-    <div class="friend-item">
-        <a class="pointer" href="/user/{{ $friend->user->id }}" target="_blank">
-            <div class="user-image">
-                @if(!is_null($friend->user->image_id))
-                    <img src="/storage/user-profile-images/{{ $friend->user->image_id }}" class="img-circle" width="80px" height="80px"/>
-                @else
-                    <img src="/storage/user-profile-images/user.png" class="img-circle" width="80px" height="80px"/>
-                @endif
-            </div>
-            <div class="user-hashtag">
-                    {{ $friend->user->hashtag }}
-            </div>
-        </a>
-    </div>
-    @endforeach
+    @if(!is_null($friends))
+        @foreach($friends as $friend)
+        <div class="friend-item">
+            <a class="pointer" href="/user/{{ $friend->user->id }}" target="_blank">
+                <div class="user-image">
+                    @if(!is_null($friend->user->image_id))
+                        <img src="/storage/user-profile-images/{{ $friend->user->image_id }}" class="img-circle" width="80px" height="80px"/>
+                    @else
+                        <img src="/storage/user-profile-images/user.png" class="img-circle" width="80px" height="80px"/>
+                    @endif
+                </div>
+                <div class="user-hashtag">
+                        {{ $friend->user->hashtag }}
+                </div>
+            </a>
+        </div>
+        @endforeach
+    @endif    
 </div>
 @endif
 
@@ -37,22 +39,23 @@
         @endif
 
     </div>
-    @foreach($followers as $follower)
-        <div class="friend-item">
-            <a class="pointer" href="/user/{{ $follower->follower->id }}" target="_blank">
-                <div class="user-image">
-                    @if(!is_null($follower->follower->image_id))
-                        <img src="/storage/user-profile-images/{{ $follower->follower->image_id }}" class="img-circle" width="80px" height="80px"/>
-                    @else
-                        <img src="/storage/user-profile-images/user.png" class="img-circle" width="80px" height="80px"/>
-                    @endif
-                </div>
-                <div class="user-hashtag">
-                        {{ $follower->follower->hashtag }}
-                </div>
-            </a>
-        </div>
-
-    @endforeach
+    @if(!is_null($followers))
+        @foreach($followers as $follower)
+            <div class="friend-item">
+                <a class="pointer" href="/user/{{ $follower->follower->id }}" target="_blank">
+                    <div class="user-image">
+                        @if(!is_null($follower->follower->image_id))
+                            <img src="/storage/user-profile-images/{{ $follower->follower->image_id }}" class="img-circle" width="80px" height="80px"/>
+                        @else
+                            <img src="/storage/user-profile-images/user.png" class="img-circle" width="80px" height="80px"/>
+                        @endif
+                    </div>
+                    <div class="user-hashtag">
+                            {{ $follower->follower->hashtag }}
+                    </div>
+                </a>
+            </div>
+        @endforeach
+    @endif
 </div>
 @endif
