40d03d12ede3e6af7b2012b818eb3dfcc8907e97
Mr. CaT <mrcat323@gmail.com>
2020-03-17 10:54:15 +0500

    Rewriting navbar

--- resources/views/layouts/includes/navbar.blade.php
+++ resources/views/layouts/includes/navbar.blade.php
@@ -1,62 +1,53 @@
 @section('navbar')
-    @php
-    $user = Auth::user();
-    @endphp
-<section class="nav-container">
-    <nav class="navbar navbar-default navbar-fixed-top navbar-netchits">
-        <div class="container-fluid">
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+	<div class="container">
+	    <a class="navbar-brand" href="{{ url('/') }}">
+		{{ config('app.name', 'Laravel') }}
+	    </a>
+	    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+		<span class="navbar-toggler-icon"></span>
+	    </button>
 
-            <div class="navbar-header">
-                <a class="navbar-brand" href="/">NetChits</a>
-            </div>
+	    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+		<!-- Left Side Of Navbar -->
+		<ul class="navbar-nav mr-auto">
 
-            <ul class="nav navbar-nav navbar-right first-navbar-ul">
-                @if(!is_null(@$user))
-                    <li>
-                        <a class="pointer" href="/user/{{ $user->id }}" target="_blank">
-                            <img src="/storage/user-profile-images/{{ $user->image_id }}" class="img-circle" width="30px" height="30px"/>
-                        </a>
-                    </li>
-                    <li>
-                        <a class="pointer" href="/user/{{ $user->id }}" target="_blank">
-                            <span>{{ @$user->email}}</span>
-                        </a>
-                    </li>
+		</ul>
 
-                    <li>
-			<form method="POST" action="{{ route('logout') }}">
-			    @csrf
-			    <button class="btn btn-link">@lang('main.signout')</button>
-			</form>
-                    </li>
-                @else
-                    <li>
-			<a class="pointer" id="signin-button" data-option="noauth">
-                            <span>@lang('main.login')</span>
+		<!-- Right Side Of Navbar -->
+		<ul class="navbar-nav ml-auto">
+		    <!-- Authentication Links -->
+		    @guest
+		    <li class="nav-item">
+			<a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+		    </li>
+		    @if (Route::has('register'))
+			<li class="nav-item">
+			    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+			</li>
+		    @endif
+        @else
+		    <li class="nav-item dropdown">
+			<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+			    {{ Auth::user()->name }} <span class="caret"></span>
 			</a>
-                    </li>
-                    <li>
-			<a class="pointer" id="signout-button">
-                            <span>@lang('main.signup')</span>
-			</a>
-                    </li>
-                @endif
-            </ul>
 
-            <div class="input-group navbar-search">
-                <input type="text" class="form-control" placeholder="follow by typing #hovdev" id="input-navbar-search">
-                <div class="input-group-btn">
-                    <button class="btn btn-default" type="submit">
-			<i class="glyphicon glyphicon-search"></i>
-                    </button>
-                </div>
-            </div>
-            @if(!is_null(@$user))
-                <a class="pointer hidden-user-image" href="/user/{{ $user->id }}" target="_blank">
-                    <img src="/storage/user-profile-images/{{ $user->image_id }}" class="img-circle" width="30px" height="30px"/>
-                </a>
-            @endif
-        </div>
+			<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+			    <a class="dropdown-item" href="{{ route('settings') }}">Settings</a>
+			    <a class="dropdown-item" href="{{ route('logout') }}"
+			       onclick="event.preventDefault();
+               document.getElementById('logout-form').submit();">
+				{{ __('Logout') }}
+			    </a>
+
+			    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+				@csrf
+			    </form>
+			</div>
+		    </li>
+		    @endguest
+		</ul>
+	    </div>
+	</div>
     </nav>
-</section>
 @show
