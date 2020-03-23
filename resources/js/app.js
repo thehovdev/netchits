b23d93b74e5fb6687e2450a182765a657b4f1c6a
Mr. CaT <mrcat323@gmail.com>
2020-03-23 23:11:41 +0500

    Fixing by removing

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,2 @@
 require('./bootstrap');
-
-$(document).on('click', '.forgot-button', function () {
-    $('.signin-container').hide();
-    $('.forgotpass-container').show();
-});
+require('./addons');
