32dcf24142e2533d3fdc5cb651d12f75c5f71f5f
Mr. CaT <mrcat323@gmail.com>
2020-04-07 11:29:22 +0500

    Binding group and chit to user model

--- app/User.php
+++ app/User.php
@@ -36,4 +36,14 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function groups()
+    {
+        return $this->hasMany(Group::class);
+    }
+
+    public function chits()
+    {
+        return $this->hasMany(Chit::class);
+    }
 }
