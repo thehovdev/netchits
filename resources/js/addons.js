1dc6d10c763a840024aab9ccf74daa1e913ec827
Mr. CaT <mrcat323@gmail.com>
2020-04-08 23:17:42 +0500

    Remake of chit adding
    When for example there is no group to add to, automatically group is created and chit placed into it, and is added dynamically to the view

--- resources/js/addons.js
+++ resources/js/addons.js
@@ -40,7 +40,11 @@ Api = {
 	    data: { address, groupId }
 	}).done(function (res) {
 	    if (res.status == 1) {
-		$('.row.row-chits-list#group-' + res.groupId + '-list').prepend(res.html);
+		if (!$('#group-' + res.groupId).length) {
+		    $('#main').append('<div class="row row-group" id="group-' + res.groupId + '"><div class="panel panel-default panel-group"><div class="panel-body">Default<i class="fa fa-window-close fa-delete-group chits-group-delete-button" id="' + res.groupId + '" aria-hidden="true"></i></div></div></div><div class="row row-chits-list" id="group-' + res.groupId + '-list"></div>');
+		}
+		$('#group-' + res.groupId + '-list').prepend(res.html);
+		
 	    } else {
 		$('.alerts').append('<div class="alert alert-danger" id="alert-' + res.id +'">' + res.message + '</div>');
 		setTimeout(function () {
